f4833facbc9436b66937b17f740e99d9c3bfb715
Soma Nagy <nagysomabalint@gmail.com>
2021-02-20 17:31:00 +0100

    added notebook

---
+++ kg_tools/kg_creation.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from wikidata.client import Client
+
+
+def triplet_for_id(_id):
+    client = Client()
+
+    entity = client.get(_id, load=True)
+
+    triplets = []
+
+    if 'descriptions' in entity.attributes:
+        if 'hu' in entity.attributes['descriptions']:
+            triplets.append((_id, 'T_description', entity.attributes['descriptions']['hu']['value']))
+
+    if 'aliases' in entity.attributes:
+        if 'hu' in entity.attributes['aliases']:
+            for a in entity.attributes['aliases']['hu']:
+                triplets.append((_id, 'T_alias', a['value']))
+
+    for k, v in entity.attributes['claims'].items():
+        try:
+            for lv in v:
+                triplets.append((_id, k, lv['mainsnak']['datavalue']['value']))
+        except:
+            pass
+
+    return pd.DataFrame(triplets, columns=['source', 'edge', 'destination'])
+
+
+def get_edge_properties(edge_id):
+    client = Client()
+    prop = client.get(edge_id, load=True)
+
+    name, description = '', ''
+    aliases = []
+    if 'hu' in prop.attributes['labels']:
+        name = prop.attributes['labels']['hu']['value']
+
+    if 'hu' in prop.attributes['descriptions']:
+        description = prop.attributes['descriptions']['hu']['value']
+
+    if 'hu' in prop.attributes['aliases']:
+        aliases = [a['value'] for a in prop.attributes['aliases']['hu']]
+
+    return {
+        'wikidata_id': edge_id,
+        'name': name,
+        'description': description,
+        'aliases': aliases
+    }
--- kg_tools/wikitools.py
+++ kg_tools/wikitools.py
@@ -4,18 +4,15 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 
-BASE_CAT_URL = 'https://hu.wikipedia.org/w/index.php?title=Kateg%C3%B3ria:Magyar_k%C3%B6lt%C5%91k&from=A'
-
-
 def get_entities_of_category(category_url, url_prefix='https://hu.wikipedia.org'):
     resp = requests.get(category_url)
-    soup = BeautifulSoup(resp.text)
+    soup = BeautifulSoup(resp.text, features="lxml")
     page_urls = [a['href'] for a in soup.find('td', {'class': 'hlist'}).find_all('a')]
 
     entities = []
     for url in page_urls:
         resp = requests.get(url)
-        soup = BeautifulSoup(resp.text)
+        soup = BeautifulSoup(resp.text, features="lxml")
         res = [(a['title'], a['href']) for a in
                soup.find('div', {'id': 'mw-pages'}).find_all('a', href=lambda s: s.startswith('/wiki/') if s else '')]
         entities.extend(res)
@@ -27,13 +24,12 @@ def get_entities_of_category(category_url, url_prefix='https://hu.wikipedia.org'
     for name, url_suffix in entities:
         try:
             res = requests.get(url_prefix + url_suffix)
-            soup = BeautifulSoup(res.text)
+            soup = BeautifulSoup(res.text, features="lxml")
             wikidata_id = soup.find('li', {'id': 't-wikibase'}).find('a')['href']
-            wiki_text = soup.find('div', {'class': 'mw-parser-output'}).text
-            res_list.append((name, url_suffix, wikidata_id, wiki_text))
+            res_list.append((name, url_suffix, wikidata_id))
         except:
             print(name, 'failed.')
 
-    entity_df = pd.DataFrame(res_list, columns=['name', 'wiki_url', 'wikidata_url', 'wiki_text'])
+    entity_df = pd.DataFrame(res_list, columns=['name', 'wiki_url', 'wikidata_url'])
 
     return entity_df
